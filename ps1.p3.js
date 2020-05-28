19b4fba2e202ef34802bf81f5d5798e2b410670b
ParkerStone <parkerwstone@gmail.com>
2020-05-28 16:36:04 -0700

    completed problem 3

--- ps1.p3.js
+++ ps1.p3.js
@@ -1,1 +1,17 @@
+function problemThree(input, createOutput) {
+    return createOutput(input);
+}
 
+console.log(problemThree('supercalifragilisticexpialidocious', (input) => {
+    return input.split(/(?=c)/g)
+}));
+
+console.log(problemThree('supercalifragilisticexpialidocious', (input) => {
+    stringObject = {
+        originalString: input,
+        modifiedString: input.replace(/a/g, 'A'),
+        numberReplaced: input.split('a').length - 1,
+        length: input.length
+    }
+    return stringObject
+}));
