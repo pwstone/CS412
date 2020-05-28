79ec224c6f27063176bac908bbffe4b1c8e15b22
ParkerStone <parkerwstone@gmail.com>
2020-05-28 15:22:11 -0700

    completed problem 2

--- ps1.p2.js
+++ ps1.p2.js
@@ -1,1 +1,24 @@
+function findOperator(input) {
+    const stringArray = input.split('');
+    const left = Number(stringArray[0]);
+    const operator = stringArray[1];
+    const right = Number(stringArray[2]);
+    switch(operator) {
+        case '+':
+            return left + right;
+        case '-':
+            return left - right;
+        case '*':
+            return left * right;
+        case '/':
+            return left / right;
+        case '%':
+            return left % right;
+    }
+}
+
+console.log(`5 * 7 = ${findOperator('5*7')}`)
+console.log(`9 % 3 = ${findOperator('9%3')}`)
+console.log(`2 + 8 = ${findOperator('2+8')}`)
+
 
