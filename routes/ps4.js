89f83819c6b57136f3c5c490905e252f7d18c53b
ParkerStone <parkerwstone@gmail.com>
2020-06-21 14:39:59 -0700

    Adjusted to Redis

--- routes/ps4.js
+++ routes/ps4.js
@@ -3,11 +3,31 @@ const express = require('express');
 const router = express.Router();
 const CONFIG = require('../configs/ps4API');
 
+const redis = require('redis');
+const client = redis.createClient();
+
 router.route('/ps4')
     .post(async (req, res, next) => {
-        let result = await fetch(CONFIG.url + '?q=' + req.body.topic + CONFIG.key);
-        let news = await result.json();
-        res.render('news', {author: news.articles[0].author, title: news.articles[0].title, body: news.articles[0].description});
+        let results = await fetch(CONFIG.url + '?q=' + req.body.topic + CONFIG.key);
+        let news = await results.json();
+        const redisKey = JSON.stringify(news);
+        client.exists(redisKey, (err, match) => {
+            if (match) {
+                res.json({
+                    result: news.articles[0].author + news.articles[0].title + news.articles[0].description,
+                    cache: true
+                })
+            } else {
+                client.set(JSON.stringify(news), JSON.stringify(news));
+                client.expire(JSON.stringify(news), 30);
+                res.json({
+                    result: news.articles[0].author + news.articles[0].title + news.articles[0].description,
+                    cache: false
+                })
+            }
+        })
     })
+
+
 
 module.exports = router;
